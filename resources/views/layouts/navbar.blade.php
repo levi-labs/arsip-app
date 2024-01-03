b2206e6025615165d326b7e3b957f339d55d4fdd
v-legacy <rezkypahlepy@rezkys-MacBook-Pro.local>
2024-01-03 16:41:25 +0700

    fix auth level

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -64,10 +64,13 @@
                     <a href="javascript:" class="nav-link "><span class="pcoded-micon"><i
                                 class="feather icon-box"></i></span><span class="pcoded-mtext">Data Master</span></a>
                     <ul class="pcoded-submenu">
-                        <li
-                            class="{{ request()->is('kategori', 'tambah-kategori', 'edit-kategori/*') ? 'active' : '' }}">
-                            <a href="{{ url('/kategori') }}" class="">Kategori</a>
-                        </li>
+                        @if (auth()->user()->level == 'Admin')
+                            <li
+                                class="{{ request()->is('kategori', 'tambah-kategori', 'edit-kategori/*') ? 'active' : '' }}">
+                                <a href="{{ url('/kategori') }}" class="">Kategori</a>
+                            </li>
+                        @endif
+
                         <li
                             class="{{ request()->is('barang', 'tambah-barang', 'edit-barang/*', 'detail-barang/*') ? 'active' : '' }}">
                             <a href="{{ url('/barang') }}" class="">Barang</a>
@@ -101,59 +104,67 @@
 
                     </ul>
                 </li>
-                <li class="nav-item pcoded-menu-caption">
-                    <label>Supplier & Cabang</label>
-                </li>
-                <li data-username="form elements advance componant validation masking wizard picker select"
-                    class="nav-item {{ request()->is('supplier', 'tambah-supplier', 'edit-supplier/*') ? 'active' : '' }}">
-                    <a href="{{ url('/supplier') }}" class="nav-link "><span class="pcoded-micon"><i
-                                class="feather icon-file-text"></i></span><span class="pcoded-mtext">Supplier</span></a>
-                </li>
-                <li data-username="Table bootstrap datatable footable"
-                    class="nav-item {{ request()->is('cabang', 'tambah-cabang', 'edit-cabang/*') ? 'active' : '' }}">
-                    <a href="{{ url('/cabang') }}" class="nav-link "><span class="pcoded-micon"><i
-                                class="feather icon-server"></i></span><span class="pcoded-mtext">Cabang</span></a>
-                </li>
-                </li>
-                <li class="nav-item pcoded-menu-caption">
-                    <label>Arsip Masuk & Keluar</label>
-                </li>
-                <li data-username="form elements advance componant validation masking wizard picker select"
-                    class="nav-item {{ request()->is('arsip-masuk', 'post-arsip-masuk', 'detail-arsip-masuk/*') ? 'active' : '' }} ">
-                    <a href="{{ url('/arsip-masuk') }}" class="nav-link "><span class="pcoded-micon"><i
-                                class="feather icon-arrow-down"></i></span><span class="pcoded-mtext">Arsip
-                            Masuk</span></a>
-                </li>
-                <li data-username="form elements advance componant validation masking wizard picker select"
-                    class="nav-item {{ request()->is('arsip-keluar', 'post-arsip-keluar', 'detail-arsip-keluar/*') ? 'active' : '' }}">
-                    <a href="{{ url('/arsip-keluar') }}" class="nav-link "><span class="pcoded-micon"><i
-                                class="feather icon-arrow-up"></i></span><span class="pcoded-mtext">Arsip
-                            Keluar</span></a>
-                </li>
+                @if (auth()->user()->level == 'Admin')
+                    <li class="nav-item pcoded-menu-caption">
+                        <label>Supplier & Cabang</label>
+                    </li>
+                    <li data-username="form elements advance componant validation masking wizard picker select"
+                        class="nav-item {{ request()->is('supplier', 'tambah-supplier', 'edit-supplier/*') ? 'active' : '' }}">
+                        <a href="{{ url('/supplier') }}" class="nav-link "><span class="pcoded-micon"><i
+                                    class="feather icon-file-text"></i></span><span
+                                class="pcoded-mtext">Supplier</span></a>
+                    </li>
+                    <li data-username="Table bootstrap datatable footable"
+                        class="nav-item {{ request()->is('cabang', 'tambah-cabang', 'edit-cabang/*') ? 'active' : '' }}">
+                        <a href="{{ url('/cabang') }}" class="nav-link "><span class="pcoded-micon"><i
+                                    class="feather icon-server"></i></span><span class="pcoded-mtext">Cabang</span></a>
+                    </li>
 
-                <li class="nav-item pcoded-menu-caption">
-                    <label>User Management</label>
-                </li>
-                <li data-username="form elements advance componant validation masking wizard picker select"
-                    class="nav-item {{ request()->is('daftar-user', 'tambah-user', 'edit-user/*') ? 'active' : '' }}">
-                    <a href="{{ url('/daftar-user') }}" class="nav-link "><span class="pcoded-micon"><i
-                                class="feather icon-users"></i></span><span class="pcoded-mtext">Daftar User</span></a>
-                </li>
-                <li class="nav-item pcoded-menu-caption">
-                    <label>Report</label>
-                </li>
-                <li data-username="form elements advance componant validation masking wizard picker select"
-                    class="nav-item {{ request()->is('report-masuk', 'post-report-masuk') ? 'active' : '' }}">
-                    <a href="{{ url('/report-masuk') }}" class="nav-link "><span class="pcoded-micon"><i
-                                class="feather icon-trending-down"></i></span><span class="pcoded-mtext">Report
-                            Masuk</span></a>
-                </li>
-                <li data-username="form elements advance componant validation masking wizard picker select"
-                    class="nav-item {{ request()->is('report-keluar', 'post-report-keluar') ? 'active' : '' }}">
-                    <a href="{{ url('/report-keluar') }}" class="nav-link "><span class="pcoded-micon"><i
-                                class="feather icon-trending-up"></i></span><span class="pcoded-mtext">Report
-                            Keluar</span></a>
-                </li>
+                    <li class="nav-item pcoded-menu-caption">
+                        <label>Arsip Masuk & Keluar</label>
+                    </li>
+                    <li data-username="form elements advance componant validation masking wizard picker select"
+                        class="nav-item {{ request()->is('arsip-masuk', 'post-arsip-masuk', 'detail-arsip-masuk/*') ? 'active' : '' }} ">
+                        <a href="{{ url('/arsip-masuk') }}" class="nav-link "><span class="pcoded-micon"><i
+                                    class="feather icon-arrow-down"></i></span><span class="pcoded-mtext">Arsip
+                                Masuk</span></a>
+                    </li>
+                    <li data-username="form elements advance componant validation masking wizard picker select"
+                        class="nav-item {{ request()->is('arsip-keluar', 'post-arsip-keluar', 'detail-arsip-keluar/*') ? 'active' : '' }}">
+                        <a href="{{ url('/arsip-keluar') }}" class="nav-link "><span class="pcoded-micon"><i
+                                    class="feather icon-arrow-up"></i></span><span class="pcoded-mtext">Arsip
+                                Keluar</span></a>
+                    </li>
+
+                    <li class="nav-item pcoded-menu-caption">
+                        <label>User Management</label>
+                    </li>
+                    <li data-username="form elements advance componant validation masking wizard picker select"
+                        class="nav-item {{ request()->is('daftar-user', 'tambah-user', 'edit-user/*') ? 'active' : '' }}">
+                        <a href="{{ url('/daftar-user') }}" class="nav-link "><span class="pcoded-micon"><i
+                                    class="feather icon-users"></i></span><span class="pcoded-mtext">Daftar
+                                User</span></a>
+                    </li>
+
+
+                    <li class="nav-item pcoded-menu-caption">
+                        <label>Report</label>
+                    </li>
+                    <li data-username="form elements advance componant validation masking wizard picker select"
+                        class="nav-item {{ request()->is('report-masuk', 'post-report-masuk') ? 'active' : '' }}">
+                        <a href="{{ url('/report-masuk') }}" class="nav-link "><span class="pcoded-micon"><i
+                                    class="feather icon-trending-down"></i></span><span class="pcoded-mtext">Report
+                                Masuk</span></a>
+                    </li>
+                    <li data-username="form elements advance componant validation masking wizard picker select"
+                        class="nav-item {{ request()->is('report-keluar', 'post-report-keluar') ? 'active' : '' }}">
+                        <a href="{{ url('/report-keluar') }}" class="nav-link "><span class="pcoded-micon"><i
+                                    class="feather icon-trending-up"></i></span><span class="pcoded-mtext">Report
+                                Keluar</span></a>
+                    </li>
+                @endif
+
+
 
             </ul>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -27,105 +27,108 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('layouts.main');
+    return view('pages.auth.login');
+});
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', [DashboardController::class,  'index']);
+    //Supplier
+    Route::get('/supplier', [SupplierController::class, 'index']);
+    Route::get('/tambah-supplier', [SupplierController::class, 'create']);
+    Route::post('/post-supplier', [SupplierController::class, 'store']);
+    Route::get('/edit-supplier/{supplier}', [SupplierController::class, 'edit']);
+    Route::post('update-supplier/{supplier}', [SupplierController::class, 'update']);
+    Route::get('/hapus-supplier/{supplier}', [SupplierController::class, 'destroy']);
+
+    //Cabang
+    Route::get('/cabang', [CabangController::class, 'index']);
+    Route::get('/tambah-cabang', [CabangController::class, 'create']);
+    Route::post('/post-cabang', [CabangController::class, 'store']);
+    Route::get('/edit-cabang/{cabang}', [CabangController::class, 'edit']);
+    Route::post('/update-cabang/{cabang}', [CabangController::class, 'update']);
+    Route::get('/hapus-cabang/{cabang}', [CabangController::class, 'destroy']);
+
+    //kategori
+    Route::get('/kategori', [KategoriController::class, 'index']);
+    Route::get('/tambah-kategori', [KategoriController::class, 'create']);
+    Route::post('/post-kategori', [KategoriController::class, 'store']);
+    Route::get('/edit-kategori/{kategori}', [KategoriController::class, 'edit']);
+    Route::post('/update-kategori/{kategori}', [KategoriController::class, 'update']);
+    Route::get('/hapus-kategori/{kategori}', [KategoriController::class, 'destroy']);
+
+    //Barang
+    Route::get('/barang', [BarangController::class, 'index']);
+    Route::get('/tambah-barang', [BarangController::class, 'create']);
+    Route::post('/post-barang', [BarangController::class, 'store']);
+    Route::get('/detail-barang/{barang}', [BarangController::class, 'show']);
+    Route::get('/edit-barang/{barang}', [BarangController::class, 'edit']);
+    Route::post('/update-barang/{barang}', [BarangController::class, 'update']);
+    Route::get('/hapus-barang/{barang}', [BarangController::class, 'destroy']);
+
+    //Barang Masuk
+    Route::controller(BarangMasukController::class)->group(function () {
+        Route::get('/barang-masuk', 'index');
+        Route::get('/daftar-detail-barang-masuk/{params}', 'listDetailItem');
+        Route::get('/pilih-sumber-barang', 'createCabangOrSupplier');
+        Route::get('/tambah-barang-masuk', 'create');
+        Route::post('/post-barang-masuk', 'store');
+        Route::get('/detail-barang-masuk/{barangMasuk}', 'show');
+        Route::get('/edit-barang-masuk/{barangMasuk}', 'edit');
+        Route::post('/update-barang-masuk/{barangMasuk}', 'update');
+        Route::get('/hapus-barang-masuk/{barangMasuk}', 'destroy');
+    });
+
+    //Barang Keluar
+    Route::controller(BarangKeluarController::class)->group(function () {
+        Route::get('/barang-keluar', 'index');
+        Route::get('/daftar-detail-barang-keluar/{params}', 'listDetailItem');
+        Route::get('/pilih-tujuan-barang', 'createCabangOrCustomer');
+        Route::get('/tambah-barang-keluar', 'create');
+        Route::post('/post-barang-keluar', 'store');
+        Route::get('/detail-barang-keluar/{barangKeluar}', 'show');
+        Route::get('/edit-barang-keluar/{barangKeluar}', 'edit');
+        Route::post('/update-barang-keluar/{barangKeluar}', 'update');
+        Route::get('/hapus-barang-keluar/{barangKeluar}', 'destroy');
+    });
+    Route::controller(ArsipMasukController::class)->group(function () {
+        Route::get('arsip-masuk', 'index');
+        Route::post('arsip-masuk', 'index');
+        Route::post('post-arsip-masuk', 'store');
+        Route::get('detail-arsip-masuk/{barangMasuk}', 'detailArsip');
+    });
+
+    Route::controller(ArsipKeluarController::class)->group(function () {
+        Route::get('arsip-keluar', 'index');
+        Route::post('arsip-keluar', 'index');
+        Route::post('post-arsip-keluar', 'store');
+        Route::get('detail-arsip-keluar/{barangKeluar}', 'detailArsip');
+    });
+
+    //Users Management
+
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/daftar-user', 'index');
+        Route::get('/tambah-user', 'create');
+        Route::post('/post-user', 'store');
+        Route::get('/edit-user/{user}', 'edit');
+        Route::post('/update-user/{user}', 'update');
+        Route::get('/hapus-user/{user}', 'destroy');
+        Route::get('/reset-password/{user}', 'resetPassword');
+        Route::get('/ubah-password', 'editPassword');
+        Route::post('/ubah-password', 'updatePassword');
+    });
+
+    Route::controller(ReportController::class)->group(function () {
+        Route::get('/report-masuk', 'formReportMasuk');
+        Route::post('/post-report-masuk', 'sendReportMasuk');
+        Route::get('/report-keluar', 'formReportKeluar');
+        Route::post('/post-report-keluar', 'sendReportKeluar');
+    });
 });
 
-Route::get('/dashboard', [DashboardController::class,  'index']);
-//Supplier
-Route::get('/supplier', [SupplierController::class, 'index']);
-Route::get('/tambah-supplier', [SupplierController::class, 'create']);
-Route::post('/post-supplier', [SupplierController::class, 'store']);
-Route::get('/edit-supplier/{supplier}', [SupplierController::class, 'edit']);
-Route::post('update-supplier/{supplier}', [SupplierController::class, 'update']);
-Route::get('/hapus-supplier/{supplier}', [SupplierController::class, 'destroy']);
 
-//Cabang
-Route::get('/cabang', [CabangController::class, 'index']);
-Route::get('/tambah-cabang', [CabangController::class, 'create']);
-Route::post('/post-cabang', [CabangController::class, 'store']);
-Route::get('/edit-cabang/{cabang}', [CabangController::class, 'edit']);
-Route::post('/update-cabang/{cabang}', [CabangController::class, 'update']);
-Route::get('/hapus-cabang/{cabang}', [CabangController::class, 'destroy']);
 
-//kategori
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/tambah-kategori', [KategoriController::class, 'create']);
-Route::post('/post-kategori', [KategoriController::class, 'store']);
-Route::get('/edit-kategori/{kategori}', [KategoriController::class, 'edit']);
-Route::post('/update-kategori/{kategori}', [KategoriController::class, 'update']);
-Route::get('/hapus-kategori/{kategori}', [KategoriController::class, 'destroy']);
 
-//Barang
-Route::get('/barang', [BarangController::class, 'index']);
-Route::get('/tambah-barang', [BarangController::class, 'create']);
-Route::post('/post-barang', [BarangController::class, 'store']);
-Route::get('/detail-barang/{barang}', [BarangController::class, 'show']);
-Route::get('/edit-barang/{barang}', [BarangController::class, 'edit']);
-Route::post('/update-barang/{barang}', [BarangController::class, 'update']);
-Route::get('/hapus-barang/{barang}', [BarangController::class, 'destroy']);
-
-//Barang Masuk
-Route::controller(BarangMasukController::class)->group(function () {
-    Route::get('/barang-masuk', 'index');
-    Route::get('/daftar-detail-barang-masuk/{params}', 'listDetailItem');
-    Route::get('/pilih-sumber-barang', 'createCabangOrSupplier');
-    Route::get('/tambah-barang-masuk', 'create');
-    Route::post('/post-barang-masuk', 'store');
-    Route::get('/detail-barang-masuk/{barangMasuk}', 'show');
-    Route::get('/edit-barang-masuk/{barangMasuk}', 'edit');
-    Route::post('/update-barang-masuk/{barangMasuk}', 'update');
-    Route::get('/hapus-barang-masuk/{barangMasuk}', 'destroy');
-});
-
-//Barang Keluar
-Route::controller(BarangKeluarController::class)->group(function () {
-    Route::get('/barang-keluar', 'index');
-    Route::get('/daftar-detail-barang-keluar/{params}', 'listDetailItem');
-    Route::get('/pilih-tujuan-barang', 'createCabangOrCustomer');
-    Route::get('/tambah-barang-keluar', 'create');
-    Route::post('/post-barang-keluar', 'store');
-    Route::get('/detail-barang-keluar/{barangKeluar}', 'show');
-    Route::get('/edit-barang-keluar/{barangKeluar}', 'edit');
-    Route::post('/update-barang-keluar/{barangKeluar}', 'update');
-    Route::get('/hapus-barang-keluar/{barangKeluar}', 'destroy');
-});
-
-Route::controller(ArsipMasukController::class)->group(function () {
-    Route::get('arsip-masuk', 'index');
-    Route::post('arsip-masuk', 'index');
-    Route::post('post-arsip-masuk', 'store');
-    Route::get('detail-arsip-masuk/{barangMasuk}', 'detailArsip');
-});
-
-Route::controller(ArsipKeluarController::class)->group(function () {
-    Route::get('arsip-keluar', 'index');
-    Route::post('arsip-keluar', 'index');
-    Route::post('post-arsip-keluar', 'store');
-    Route::get('detail-arsip-keluar/{barangKeluar}', 'detailArsip');
-});
-
-//Users Management
-
-Route::controller(UserController::class)->group(function () {
-    Route::get('/daftar-user', 'index');
-    Route::get('/tambah-user', 'create');
-    Route::post('/post-user', 'store');
-    Route::get('/edit-user/{user}', 'edit');
-    Route::post('/update-user/{user}', 'update');
-    Route::get('/hapus-user/{user}', 'destroy');
-    Route::get('/reset-password/{user}', 'resetPassword');
-    Route::get('/ubah-password', 'editPassword');
-    Route::post('/ubah-password', 'updatePassword');
-});
-
-Route::controller(ReportController::class)->group(function () {
-    Route::get('/report-masuk', 'formReportMasuk');
-    Route::post('/post-report-masuk', 'sendReportMasuk');
-    Route::get('/report-keluar', 'formReportKeluar');
-    Route::post('/post-report-keluar', 'sendReportKeluar');
-});
-
-Route::get('sign-out' , function (){
+Route::get('sign-out', function () {
     Auth::logout();
     return redirect('login');
 });
